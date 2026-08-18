78f21dd1546b37adf3274281651c0942b26bdb05
Rajkumar J <rajkumar.work.in@gmail.com>
2026-08-17 20:18:57 -0700

    updated the timeslot controller

--- src/main/java/com/example/slotsecure_backend/controller/TimeSlotController.java
+++ src/main/java/com/example/slotsecure_backend/controller/TimeSlotController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/TimeSlots")
+@RequestMapping("/api/timeslots")
 public class TimeSlotController {
 
     @Autowired
