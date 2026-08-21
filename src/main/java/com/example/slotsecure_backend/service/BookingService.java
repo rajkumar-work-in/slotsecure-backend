6108f36d375b9cd5ef22214c4086d64944fff270
Rajkumar J <rajkumar.work.in@gmail.com>
2026-08-21 02:19:24 -0700

    updated slot secure exception

--- src/main/java/com/example/slotsecure_backend/service/BookingService.java
+++ src/main/java/com/example/slotsecure_backend/service/BookingService.java
@@ -27,7 +27,7 @@ public class BookingService {
     public Booking createBooking(Long userId, Long timeSlotId) {
 
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException(""));
+                .orElseThrow(() -> new RuntimeException("User not found!"));
 
         TimeSlot timeSlot = timeSlotRepository.findById(timeSlotId)
                 .orElseThrow(() -> new RuntimeException("TimeSlot not found!"));
