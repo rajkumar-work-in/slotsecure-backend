423a7cc033261567a6dc7b2383f2f729553c329d
Rajkumar J <rajkumar.work.in@gmail.com>
2026-08-24 22:47:02 -0700

    updated hashedPassword version

--- src/main/java/com/example/slotsecure_backend/service/UserService.java
+++ src/main/java/com/example/slotsecure_backend/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.slotsecure_backend.entity.User;
 import com.example.slotsecure_backend.exception.DuplicateEmailException;
 import com.example.slotsecure_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User registerUser(User user) {
         User existingUser = userRepository.findByEmail(user.getEmail());
 
         if(existingUser != null) {
             throw new DuplicateEmailException("Email already registered!");
         }
+
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
 
         return userRepository.save(user);
     }
