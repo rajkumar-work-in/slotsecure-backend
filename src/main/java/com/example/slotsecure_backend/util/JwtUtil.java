44471ca02d99e4fb984e3999e95f344cc938895f
Rajkumar J <rajkumar.work.in@gmail.com>
2026-08-26 04:52:47 -0700

    created parseSignedClaims to verify the token

--- src/main/java/com/example/slotsecure_backend/util/JwtUtil.java
+++ src/main/java/com/example/slotsecure_backend/util/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.example.slotsecure_backend.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,5 +31,30 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSigningKey())
                 .compact();
+    }
+
+    public String extractEmail(String token){
+        return extractAllClaims(token).getSubject();
+    }
+
+    public String extractRole(String token){
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public boolean isTokenValid(String token){
+        try{
+            extractAllClaims(token);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
