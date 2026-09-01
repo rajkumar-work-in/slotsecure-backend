2b3b0c2c4b9f16e7e09c01ffc50b19fcbd3a962e
Rajkumar J <rajkumar.work.in@gmail.com>
2026-08-31 22:28:47 -0700

    created role updated logic

--- src/main/java/com/example/slotsecure_backend/controller/VendorController.java
+++ src/main/java/com/example/slotsecure_backend/controller/VendorController.java
@@ -4,6 +4,7 @@ import com.example.slotsecure_backend.entity.Vendor;
 import com.example.slotsecure_backend.service.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +17,9 @@ public class VendorController {
     private VendorService vendorService;
 
     @PostMapping
-    public ResponseEntity<Vendor> createVendor(@RequestBody Vendor vendor) {
-        Vendor savedVendor = vendorService.createVendor(vendor);
+    public ResponseEntity<Vendor> createVendor(@RequestBody Vendor vendor, Authentication authentication) {
+        String email = authentication.getName();
+        Vendor savedVendor = vendorService.createVendor(vendor, email);
         return ResponseEntity.ok(savedVendor);
     }
 
--- src/main/java/com/example/slotsecure_backend/service/VendorService.java
+++ src/main/java/com/example/slotsecure_backend/service/VendorService.java
@@ -1,6 +1,9 @@
 package com.example.slotsecure_backend.service;
 
+import com.example.slotsecure_backend.entity.Role;
+import com.example.slotsecure_backend.entity.User;
 import com.example.slotsecure_backend.entity.Vendor;
+import com.example.slotsecure_backend.repository.UserRepository;
 import com.example.slotsecure_backend.repository.VendorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +16,21 @@ public class VendorService {
     @Autowired
     private VendorRepository vendorRepository;
 
-    public Vendor createVendor(Vendor vendor) {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Vendor createVendor(Vendor vendor, String email) {
+        User existingUser = userRepository.findByEmail(email);
+
+        if(existingUser == null) {
+            throw new RuntimeException("User not found!");
+        }
+
+        existingUser.setRole(Role.VENDOR);
+        userRepository.save(existingUser);
+
+        vendor.setUser(existingUser);
+
         return vendorRepository.save(vendor);
     }
 
